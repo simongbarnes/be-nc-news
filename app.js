768548c52ca97424764c4045c486bf0ae591a0af
simongbarnes <simongbarnes@gmail.com>
2023-10-05 05:24:08 +0100

    10 Get users

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { getEndpoints } = require("./controllers/endpoints.controller");
 const getTopics = require("./controllers/topics.controller");
 const { getArticleById, getArticles } = require("./controllers/articles.controller");
 const app = express();
+const getUsers = require("./controllers/users.controller");
 
 app.get("/api", getEndpoints);
 
@@ -11,6 +12,8 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 
 app.get('/api/articles', getArticles);
+
+app.get('/api/users', getUsers);
 
 app.all("/*", (req, res, next) => {
   res.status(404).send({ message: "Invalid path" });
